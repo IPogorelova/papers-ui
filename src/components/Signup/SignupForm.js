4cedc8f0d1d6353a314ff2e4c11acbb20e1a3c7b
IPogorelova <pisseversk@mail.ru>
2020-02-29 16:08:57 +0300

    add thanx modal after sign up

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {useHistory} from "react-router-dom";
 
 const REGISTER_URL = 'https://papers-api.azurewebsites.net/api/v1/User/register';
 
-const LoginForm = () => {
+const SignupForm = () => {
     const [ email, setEmail ] = React.useState('');
     const [ password, setPassword  ] = React.useState('');
+    const [ isThanxShown, setIsThanxShown ]= React.useState(false)
+    let history = useHistory();
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -14,6 +17,7 @@ const LoginForm = () => {
             password: password
         })
             .then(function ( response ) {
+                setIsThanxShown(true)
                 const { accessToken, refreshToken } = response.data
                 localStorage.clear()
                 localStorage.setItem('access', accessToken)
@@ -32,38 +36,59 @@ const LoginForm = () => {
         e.target.parentElement.removeAttribute('style');
     }
 
-    return (
-        <form className="form">
-            <label className="form__label">
-                Your e-mail:&nbsp;&nbsp;
-                <input
-                    type="email"
-                    name="email"
-                    className="form__input"
-                    onChange={e => setEmail(e.currentTarget.value)}
-                    onBlurCapture={e =>setEmail(e.currentTarget.value)}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-            </label>
-            <label className="form__label">
-                Your password:&nbsp;&nbsp;
-                <input
-                    type="password"
-                    name="password"
-                    className="form__input"
-                    onChange={e => setPassword(e.currentTarget.value)}
-                    onBlurCapture={e => setPassword(e.currentTarget.value)}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-            </label>
-            <button
-                type="submit"
-                className="button form__button"
-                onClick={onSubmit}> Sign up </button>
-        </form>
-    )
+    const handleThanx = () => {
+        history.push("/communities")
+        setEmail('')
+        setPassword('')
+    }
+
+    if (isThanxShown) {
+        return (
+            <div className="form__thanx">
+                <h1 className='subtitle'>Hooray!</h1>
+                <p>You are signed up successfully</p>
+                <button
+                    className="button form__button"
+                    onClick={handleThanx}
+                > Create community
+                </button>
+            </div>
+        )
+    } else {
+        return (
+            <form className="form">
+                <label className="form__label">
+                    Your e-mail:&nbsp;&nbsp;
+                    <input
+                        type="email"
+                        name="email"
+                        className="form__input"
+                        onChange={e => setEmail(e.currentTarget.value)}
+                        onBlurCapture={e => setEmail(e.currentTarget.value)}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    />
+                </label>
+                <label className="form__label">
+                    Your password:&nbsp;&nbsp;
+                    <input
+                        type="password"
+                        name="password"
+                        className="form__input"
+                        onChange={e => setPassword(e.currentTarget.value)}
+                        onBlurCapture={e => setPassword(e.currentTarget.value)}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    />
+                </label>
+                <button
+                    type="submit"
+                    className="button form__button"
+                    onClick={onSubmit}> Sign up
+                </button>
+            </form>
+        )
+    }
 }
 
-export default LoginForm;
+export default SignupForm;
