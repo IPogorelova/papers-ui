385df1295f50e6bd59c2482650874b2005861ebc
IPogorelova <pisseversk@mail.ru>
2020-11-29 17:35:42 +0300

    Add Sidebar logic for active items highlighting

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,31 +6,41 @@ import { ReactComponent as Members } from '../../images/icons/group.svg'
 import { ReactComponent as Settings } from '../../images/icons/settings.svg'
 
 const Sidebar = () => {
+  const [activeItem, setActiveItem] = React.useState('')
+  const [communityId, setCommunityId] = React.useState('')
+
+  React.useEffect(() => {
+    let pageHref = window.location.href.split('/')[4] ? window.location.href.split('/')[4] : window.location.href.split('/')[3];
+    let community = window.location.href.split('/')[3];
+    setActiveItem(pageHref)
+    if (community) setCommunityId(community)
+  })
+
   return (
     <aside className='sidebar'>
       <nav className='sidebar__nav sidebar-nav' aria-label='Community navigation'>
         <ul>
-          <li className='sidebar-nav__link sidebar-nav__link_active'>
-            <a href='#'>
+          <li className={`sidebar-nav__link ${activeItem.includes('about') && 'sidebar-nav__link_active'}`}>
+            <a href='/about'>
               <Overview/> Overview
             </a>
           </li>
-          <li className='sidebar-nav__link'>
-            <a href='#'>
+          <li className={`sidebar-nav__link ${activeItem.includes('requests') && 'sidebar-nav__link_active'}`}>
+            <a href={`/${communityId}/requests`}>
               <Talks/> Requests
             </a>
           </li>
-          <li className='sidebar-nav__link'>
+          <li className={`sidebar-nav__link ${activeItem.includes('process') && 'sidebar-nav__link_active'}`}>
             <a href='#'>
               <Papers/> Process board
             </a>
           </li>
-          <li className='sidebar-nav__link'>
+          <li className={`sidebar-nav__link ${activeItem.includes('team') && 'sidebar-nav__link_active'}`}>
             <a href='#'>
               <Members/> Team
             </a>
           </li>
-          <li className='sidebar-nav__link'>
+          <li className={`sidebar-nav__link ${activeItem.includes('settings') && 'sidebar-nav__link_active'}`}>
             <a href='#'>
               <Settings/> Settings
             </a>
